d0cec17d2f4da81501a343e795297238878ffc39
Murugapandian R <50978402+pandian79@users.noreply.github.com>
2021-10-14 18:53:16 +0800

    initial upload

---
+++ deletecomponent.ps1
@@ -0,0 +1,101 @@
+<#
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+	
+	.SYNOPSIS
+	  Deleting a Component - /api/eg/orchestration/deletecomponent
+	.DESCRIPTION
+	  Use this API to delete a component from the eG manager.
+		Ref: https://www.eginnovations.com/documentation/Automatically-Configuring-the-Target-Environment-using-REST-API/Deleting-a-Component.htm
+	.PARAMETER managerurl
+		URL of the eG manager. E.g., http://192.168.8.8:7077
+	.PARAMETER user
+		valid user for the eG manager
+	.PARAMETER pwd
+		base64 encoded password
+	.PARAMETER componenttype
+		Component type
+	.PARAMETER componentname
+		The nick name of the component
+	.PARAMETER port
+		Port
+	.INPUTS
+	  None
+	.OUTPUTS
+	  Shows the confirmation of the execution or error
+	.NOTES
+	  Version:        1.0
+	  Author:         pandian
+	  Creation Date:  14 Oct 2021
+	  Purpose/Change: Initial script development
+	  
+	.EXAMPLE
+	  .\deletecomponent.ps1 -manager h"https://192.168.20.30:7077" -user myuser -pwd cGFzc3dvcmQ= -componenttype "Apache Web" -componentname apachetest -port 80
+#>
+
+#-------------------get command line parameters----------------------
+param (
+	[Parameter(Mandatory=$true,ValueFromPipeline=$true)]
+	$managerurl, 
+	[Parameter(Mandatory=$true,ValueFromPipeline=$true)]
+	$user, 
+	[Parameter(Mandatory=$true,ValueFromPipeline=$true)]
+	$pwd, 
+	[Parameter(Mandatory=$true,ValueFromPipeline=$true)]
+	$componenttype, 
+	[Parameter(Mandatory=$true,ValueFromPipeline=$true)]
+	$componentname, 
+	[Parameter(Mandatory=$true,ValueFromPipeline=$true)]
+	$port,
+	$sid
+	)
+
+#-------------------assignments----------------------	
+$api = ($managerurl+'/api/eg/orchestration/deletecomponent')
+
+$header = @{
+	'managerurl' = $managerurl
+	'user' = $user
+	'pwd' = $pwd
+	'Content-Type' = 'application/json'
+}
+
+
+$body = @{
+	'componenttype' = $componenttype
+	'componentname' = $componentname
+	'port' = $port
+	'sid' = $sid
+}
+
+#-------------------processing----------------------	
+$nullVariables = [System.Collections.ArrayList]@()
+foreach($key in $body.keys)
+{
+	$value = $body[$key]
+	if ($value -eq $null){
+		$nullVariables.Add($key)
+		#Write-Output ($key+"value is null.")
+	}
+}
+
+foreach ($key in $nullVariables){
+	$body.Remove($key);
+	#Write-Output ($key+"is removed.")
+}
+
+$JsonBody = $Body | ConvertTo-Json
+
+Invoke-RestMethod -Method 'Post' -Uri $api -Body $JsonBody -Header $header -ContentType 'application/json'
+
+#>
